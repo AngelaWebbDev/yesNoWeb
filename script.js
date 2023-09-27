01b6e9904015daffd1cbc8f97cae236142ff62f1
Angela Webb <angelawebb.dev@gmail.com>
2023-09-27 18:03:28 -0400

    ready to share

--- script.js
+++ script.js
@@ -1,27 +1,33 @@
-var answerKey = ["yes", "of course", "maybe", "no", "never", "not today", "possibly", "likely","unlikely"];
+console.log('javascript loaded');
+const answerKey = ["yes", "of course", "maybe", "no", "never", "not today", "possibly", "likely","unlikely"];
 
-function getAnswer(){    
-    console.log('function entered');
+function getAnswer(){
     var question = document.querySelector("#theQuestion").value;
     question = question.toLowerCase();
     if(question.startsWith('am') || question.startsWith('is') || question.startsWith('does')
         || question.startsWith('can') || question.startsWith('are') || question.startsWith('was')
         || question.startsWith('were') || question.startsWith('has') || question.startsWith('have')
         || question.startsWith('do') || question.startsWith('did') || question.startsWith('should') 
-        || question.startswith('may')){
+        || question.startsWith('may')){
             var indexNum = Math.ceil(Math.random()*(answerKey.length-1));
-            console.log('math.ceil: ' + Math.ceil(Math.random()*(answerKey.length-1)));
-            console.log('math.floor: ' + Math.floor(Math.random()*(answerKey.length-1)));
-            console.log('math.round: ' + Math.round(Math.random()*(answerKey.length-1)));
-            console.log('indexNum = ' + indexNum);
             var answer = answerKey[indexNum];
             document.getElementById('theAnswer').innerText = answer;
         console.log("end of if");
     } else if(question ==='quit'){
-        console.log("else if");
         document.getElementById('theAnswer').innerText = "goodbye";
-    } else{
-        console.log("else");
+        setTimeout(closeProgram,1000);
+    } else if(question == ''){
+        document.getElementById('theAnswer').innerText = "You have to ask a question if you want an answer.";
+    }else{
         document.getElementById('theAnswer').innerText = 'I don\'t understand the question.';
     }
+}
+
+function closeProgram(){
+    document.getElementById('mainArea').remove();
+    document.getElementById('bodyArea').style.backgroundColor = "white";
+    document.getElementById('headerArea').remove();
+    document.getElementById('goodbyePic').style.display = "block";
+    document.getElementById('goodbyePic').style.height = "300px";
+    document.getElementById('goodbyePic').style.margin = "auto";
 }
